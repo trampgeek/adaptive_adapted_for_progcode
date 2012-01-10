b1fe201c6cb1ca16e7a8ae3a1762917ffd2e1266
Richard Lobb <richard.lobb@canterbury.ac.nz>
2012-01-10 16:33:15 +1300

    Initial git repository creation

---
+++ renderer.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(dirname(__FILE__) . '/../adaptive/renderer.php');
+
+// GRRRR. This is unnecessary.
+
+class qbehaviour_adaptive_adapted_for_pycode_renderer extends qbehaviour_adaptive_renderer
+{
+}
